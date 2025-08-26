312c89c43970ada6eef368988df819c7e6368fdd
daniakl <daniabc4@hotmail.com>
2025-08-25 20:50:36 -0400

    added favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,32 +1,35 @@
-
-import { Inter, Mona_Sans } from "next/font/google"
-import "./globals.css"
+import { Inter, Mona_Sans } from "next/font/google";
+import "./globals.css";
 
 const inter = Inter({
   variable: "--font-inter",
   subsets: ["latin"],
-})
+});
 
 const monaSans = Mona_Sans({
   variable: "--font-mona-sans",
   subsets: ["latin"],
-})
+});
 
 export default function RootLayout({
   children,
 }: Readonly<{
-  children: React.ReactNode
+  children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html>
+      <head>
+        <link rel="icon" href="/favicon.ico" sizes="any" />
+        <link rel="icon" href="/favicon.svg" type="image/svg+xml" />
+        <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
+        <meta name="theme-color" content="#E5DEF8" />
+      </head>
+
       <body
         className={`${monaSans.variable} ${inter.variable} antialiased font-sans scroll-smooth`}
       >
-        <div className="root">
-         
-          {children}
-        </div>
+        <div className="root">{children}</div>
       </body>
     </html>
-  )
+  );
 }
